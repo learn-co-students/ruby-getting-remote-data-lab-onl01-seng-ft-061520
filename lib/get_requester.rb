0d5ccc2c057a211105aca3149a47c4efbb952e6b
Carlos Rosario <carlosrosario2k@gmail.com>
2020-07-03 23:20:37 -0400

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,26 @@
+require 'net/http'
+ require 'open-uri'
+ require 'json'
+
+class GetRequester
+    def initialize(url)
+        @url = url
+    end
+
+    # url = learn open ruby-getting-remote-data-lab
+    
+    def get_response_body
+    uri = URI.parse(@url)
+    response = Net::HTTP.get_response(uri)
+    response.body
+    end
+    
+
+    def parse_json
+        JSON.parse(get_response_body)
+
+    end
+
+end
+
 # Write your code here
