fef715985f4d51d1aacfb6c31fdfc2035289c82d
Faith Gicheha <fwgicheh@gmail.com>
2020-07-04 00:38:27 -0700

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 require 'net/http'
 require 'json'
 
-class GetRequester 
+class GetRequester  #this is a class that can be initiated by passing in a URL
 
     URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
 
@@ -11,12 +11,14 @@ class GetRequester
         @url = url 
     end 
 
-    def get_response_body
+    def get_response_body #this is an instance method on the GetRequester class. 
+        #This sends a GET request to the URL provided on initialization, returning the body of the request 
             uri = URI.parse(@url)
             response = Net::HTTP.get_response(uri)
             response.body
     end 
-def parse_json
+def parse_json # is an instance methof on the GetRequester class 
+    #converts JSON returned from 'get_response_body' into Ruby data structures 
 
     JSON.parse(get_response_body)
 
